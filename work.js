22d4798fcf5404d319d362b479d2806235e118b6
Anand Patel <anand@Anands-iMac.local>
2020-12-05 14:51:18 -0500

    added styling

--- work.js
+++ work.js
@@ -8,7 +8,6 @@ $(() => {
 
 
 
-
     $.ajax({
       url: 'https://api.spotify.com/v1/me',
       headers: {
@@ -23,7 +22,12 @@ $(() => {
       $('#search-btn').on('click', () => {
       const $search_val = $('#search-val').val()
       console.log($search_val);
-
+      $('.artist-info').remove()
+      $('<div>').addClass('artist-info').appendTo($('.interface'))
+      $('.carosel-container').remove()
+      $('<div>').addClass('carosel-container').appendTo('.interface')
+      $('<div>').addClass('album-tracks').appendTo($('.interface'))
+      $('.album-tracks').remove()
 
     $.ajax({
       url: `https://api.spotify.com/v1/search?q=${$search_val}&type=artist` ,
@@ -35,8 +39,9 @@ $(() => {
         const artistId = data.artists.items[0].id
         const artistImage = data.artists.items[0].images[0].url
 
-        $('<h1>').text(artistName).appendTo($('body'))
-        $('<img>').attr('src', artistImage).appendTo($('body'))
+        $('<h1>').text(artistName).appendTo($('.artist-info'))
+        $('<img>').attr('src', artistImage).appendTo($('.artist-info'))
+
 
         // console.log(artistId);
         // console.log(data.artists.items[0].name);
@@ -50,9 +55,32 @@ $(() => {
         }).then((data) => {
           console.log(data);
           for (let i=0; i<data.items.length; i++){
+            const albumId = data.items[i].id
             const albumName = data.items[i].name
             const albumArt = $('<img>').attr('src', data.items[i].images[0].url)
-            $('<h6>').text(albumName).appendTo($('body')).append(albumArt)
+            $('<h6>').text(albumName).appendTo($('.carosel-container')).append(albumArt)
+
+            $.ajax({
+              url:`https://api.spotify.com/v1/albums/${albumId}/tracks` ,
+              headers: {
+                'Authorization': 'Bearer ' + access_token
+              }
+            }).then((data) => {
+              const $albumTracks = $('<div>').addClass('album-tracks')
+              for (i=0; i<data.items.length; i++){
+                const trackName = (data.items[i].name)
+                $('<h1>').text(trackName).appendTo($albumTracks)
+                $albumTracks.appendTo('body')
+                // console.log(trackName);
+              }
+              // console.log(trackName);
+              // console.log(' space');
+            }), (error) => {
+              console.log(error);
+            }
+
+
+
           }
         }), (error) => {
           console.log(error);
